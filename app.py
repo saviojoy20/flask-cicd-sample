2257523cffe5b7f434b6705d228bdeacf7c95d97
saviojoyk <saviojoy20@gmail.com>
2019-12-29 20:13:30 +0530

    v2

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<center><h1>Flask Application Version1.22/h1></center>"
+    return "<center><h1>Flask Application Version1.3/h1></center>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=8080)
